24c8aeabb47b5463a3752c0f3dd289b139833f44
amazzalel-habib <amazzal.elhabib@gmail.com>
2019-01-07 21:30:29 +0100

    Generating random data

--- Projet Linux/insert.py
+++ Projet Linux/insert.py
@@ -1,34 +1,39 @@
 import redis
-
+import random as r
+from datetime import datetime,timedelta
 r = redis.Redis(host='localhost',port=6379,charset='utf-8',decode_responses=True)
 
 r.flushall()
-users = {
-	'AMAZZAL':{'name':'AMAZZAL EL-HABIB','cat':'personnel'},
-	'JABBEUR':{'name':'JABBEUR Youssef','cat':'personnel'},
-	'REFAK':{'name':'REFAK Aymane','cat':'personnel'},
-	'BERRADA':{'name':'BERRADA Omar','cat':'Hôpital'}
-}
-userCompteur = {
-	'AMAZZALCompteurs':['COM1','COM2'],
-	'JABBEURCompteurs':['COM3'],
-	'REFAKCompteurs':['COM4'],
-	'BERRADACompteurs':['COM5']
-}
-compteursDetails = {
-	'COM1Adresse':'Avenue X 61 RABAT',
-	'COM2Adresse':'Avenue A 54 RABAT',
-	'COM3Adresse':'Avenue C 58 CASA',
-	'COM4Adresse':'Avenue R 84 RABAT',
-	'COM5Adresse':'Avenue Z 2 CASA'
-}
-compteurConsommation = {
-	'COM1':['2018-05-01 10:00:00.000000',0.5,'2018-05-01 10:30:00.000000',0.6,'2018-05-01 11:00:00.000000',0.1,'2018-05-02 10:00:00.000000',0.3],
-	'COM2':['2018-05-01 10:00:00.000000',0.5],
-	'COM3':['2018-05-01 10:00:00.000000',0.5],
-	'COM4':['2018-05-01 10:00:00.000000',0.5],
-	'COM5':['2018-05-01 10:00:00.000000',0.5]
-}
+numOfUsers = 100
+numOfCompteurPerClient = 5
+users = {}
+userCompteur = {}
+compteursDetails = {}
+compteurConsommation = {}
+for i in range(numOfUsers):
+	id = 'Nom'+str(i+1)
+	user = {'name':id,'cat':'Personnel'}
+	users[id] = user
+
+for i in range(numOfUsers):
+	id = 'Nom'+str(i+1)+'Compteurs'
+	compteurs = ['COM'+str(i+1)+'-'+str(j+1) for j in range(numOfCompteurPerClient)]
+	userCompteur[id] = compteurs
+	for j in range(numOfCompteurPerClient):
+		compteurID = 'COM'+str(i+1)+'-'+str(j+1)
+		compteursDetails[compteurID+'Adresse'] = 'Avenue X '+str(i+1)+str(j+1)+' RABAT'
+		numOfCons = r.randint(10,100)
+		dateNow = datetime.now()
+		startDate = dateNow-timedelta(days=5)
+		consommations = []
+		for _ in range(numOfCons):
+			date = startDate + timedelta(minutes=30)
+			valeur = float('{:.2f}'.format(r.random()*10))
+			startDate = date
+			consommations.append(date.strftime('%Y-%m-%d %H:%M:%S.%f'))
+			consommations.append(valeur)
+		compteurConsommation[compteurID] = consommations
+
 #Inserer les utilisateur IDS
 r.lpush('Users',*tuple(users.keys()))
 
